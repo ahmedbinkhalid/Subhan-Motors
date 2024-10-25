2a38bf7b248acd95ab09a992422eb46976d9b399
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-10-25 10:03:46 +0500

    Added comment for routes
    For the better understanding added comments for routes including for who they are for and what they are for

--- Backend/Routes/queryRoutes.js
+++ Backend/Routes/queryRoutes.js
@@ -3,8 +3,13 @@ const {verifyToken, isUser, isAdmin} = require('../Middlewares/middleware')
 const express = require('express');
 const router = express.Router();
 
+// Client route to submit Query
 router.post('/query',verifyToken, isUser, queryController.postQuery);
+
+// Admin Route to see all Queries
 router.get('/getQueries', verifyToken, isAdmin, queryController.getQuery);
+
+// Admin Route to view indiviual Query by Query Id
 router.get('/query/:id', verifyToken, isAdmin, queryController.getQueryById);
 
 module.exports = router;
--- Backend/Routes/sellRoutes.js
+++ Backend/Routes/sellRoutes.js
@@ -27,17 +27,40 @@ const upload = multer({
     }
 });
 
-router.post('/addcars', upload.array('images', 10), sellController.addCar);
+// User Route to post Ad for selling car
+router.post('/addcars', verifyToken, isUser, upload.array('images', 10), sellController.addCar);
+
+// Route to get all new cars
 router.get('/newcars', sellController.getNewCars);
+
+// Route to get new car by Id
 router.get('/newcars/:id', sellController.getNewCarById);
-router.post('/postcar', upload.array('images', 10), sellController.newCars);
+
+// Admin Route to add new car for sale
+router.post('/postcar', verifyToken, isAdmin, upload.array('images', 10), sellController.newCars);
+
+// Route to get all cars (used and bankreleased)
 router.get('/getcars', sellController.getAllCars);
+
+// Route to get car by id
 router.get('/getCarById/:id', sellController.getCarById);
+
+// Route to only get used cars
 router.get('/usedcars', sellController.getUsedCars);
+
+// Route to only get bank released cars
 router.get('/bankcars', sellController.getBankCars);
+
+// Route for search bar
 router.get('/searchcars/:key', sellController.SearchCars);
+
+// User Route to get Ads posted by user for selling car
 router.get('/usercars', verifyToken, isUser, sellController.getUserCars);
+
+// User route to delete car Ad
 router.delete('/deletecar/:id', verifyToken, isUser, sellController.deleteCar);
+
+// User route to update Car Ad
 router.put('/updatecar/:id', verifyToken, isUser, upload.array('images', 10), sellController.updateCar);
 
 module.exports = router;
