6acc537ccdd0eba19d4f04ec0e90580c91a8ab4e
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-10-29 15:11:04 +0500

    Updated

--- Backend/Routes/sellRoutes.js
+++ Backend/Routes/sellRoutes.js
@@ -17,7 +17,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
     storage: storage,
-    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB size limit
+    limits: { fieldSize: 10 * 1024 * 1024, fileSize: 5 * 1024 * 1024 }, // 5MB size limit
     fileFilter: function (req, file, cb) {
         // Accept images only
         if (!file.mimetype.startsWith('image/')) {
